7a496ff0a8ab740ab29f080f435ceaa41b464674
Shariq Munir <shariq.munir7@gmail.com>
2025-01-21 15:41:44 +0500

    UPdate: Remove Duplicates from Sorted Array

--- Remove Duplicates from Sorted Array/code.ts
+++ Remove Duplicates from Sorted Array/code.ts
@@ -1,13 +1,10 @@
 function removeDuplicates(nums: number[]): number {
   const result: number[] = [];
-  let k = 1;
   let res: number = nums[0];
   let i = 1;
   let j = i;
-  const ress: number[] = [];
   while (i < nums.length) {
     if (nums[i] !== res) {
-      k++;
       res = nums[i];
       const temp = nums[i];
       nums[i] = nums[j];
@@ -16,7 +13,7 @@ function removeDuplicates(nums: number[]): number {
     }
     i++;
   }
-  return k;
+  return j;
 }
 
 const nums1 = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
